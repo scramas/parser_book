fc4a9ee4f6b0db9375b93e9e04c38435567da7ad
scramas <and2321@mail.ru>
2021-05-12 22:03:02 +0300

    v 0.1.fuck

--- main.py
+++ main.py
@@ -114,11 +114,7 @@ def telegram_bot(token):
                 soup = BeautifulSoup(src, "lxml")
                 link = soup.find("a", class_='img')
                 imgs = soup.find("div", class_='row block_content').find_all('img')
-                for item in imgs:
-                    img = item.get('src')
-                    title=item.get('title')
-                    print(img)
-                    bot.send_photo(message.chat.id, img, caption=title)
+                for item            bot.send_photo(message.chat.id, img, caption=title)
 
 
                 bot.send_document(message.chat.id, f)
